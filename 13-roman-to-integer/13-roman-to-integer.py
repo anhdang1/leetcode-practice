ca34cfff96f63ba40dcdbd712788f0e6adfcf20a
Anh Dang <97132887+anhdang1@users.noreply.github.com>
2022-03-22 21:17:30 -0700

    Time: 55 ms (53.73%), Space: 13.4 MB (57.75%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        #largest to smallest: add them up
+        #smaller before larger: substract smaller
+        
+        #Create a hash map
+        roman = {"I": 1, "V" : 5, "X": 10, "L" : 50, "C" :100, "D": 500, "M": 1000}
+        res = 0
+        
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+                
+        return res
