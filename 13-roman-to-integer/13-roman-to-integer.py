8551a62340232d801343f7464ab78bb3388d4395
Anh Dang <97132887+anhdang1@users.noreply.github.com>
2022-03-24 01:10:48 -0700

    Time: 47 ms (70.15%), Space: 13.3 MB (96.20%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -8,13 +8,18 @@ class Solution:
         #smaller before larger: substract smaller
         
         #Create a hash map
-        roman = {"I": 1, "V" : 5, "X": 10, "L" : 50, "C" :100, "D": 500, "M": 1000}
+        roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         res = 0
-        
         for i in range(len(s)):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+            #check to make sure there is a number behind it
+            if i+1 < len(s) and roman[s[i]] < roman[s[i+1]]:
                 res -= roman[s[i]]
             else:
                 res += roman[s[i]]
                 
         return res
+                
+        
+        
+
+                
