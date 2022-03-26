a76f14986bcc8c0496150eba60d3054e07a1866c
Anh Dang <97132887+anhdang1@users.noreply.github.com>
2022-03-26 16:16:37 -0700

    Time: 62 ms (18.76%), Space: 14.1 MB (79.23%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target > nums[-1]:
+            return len(nums)
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+
+            
+            
+
