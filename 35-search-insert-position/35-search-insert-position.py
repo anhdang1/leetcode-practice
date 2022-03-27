bf1c76ab8d441deafa1580253b63d89bb01cb3aa
Anh Dang <97132887+anhdang1@users.noreply.github.com>
2022-03-26 22:49:42 -0700

    Time: 43 ms (59.55%), Space: 14.2 MB (54.11%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -5,11 +5,32 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        """
         if target > nums[-1]:
             return len(nums)
         for i in range(len(nums)):
             if nums[i] >= target:
                 return i
+        """
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            mid = (l+r) // 2
+            
+            if nums[mid] == target:
+                return mid
+            if target > nums[mid]:
+                l = mid + 1
+            else: #if target < nums[mid]
+                r = mid - 1
+        
+        return l
+                
+            
+                
+                
+                
+        
 
             
             
