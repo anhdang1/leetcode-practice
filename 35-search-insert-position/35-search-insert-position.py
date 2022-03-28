90c28de562a8257e16d25d0a5433454155c26685
Anh Dang <97132887+anhdang1@users.noreply.github.com>
2022-03-27 21:47:52 -0700

    Time: 63 ms (17.01%), Space: 14.3 MB (25.32%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -16,16 +16,13 @@ class Solution(object):
         
         while l <= r:
             mid = (l+r) // 2
-            
             if nums[mid] == target:
                 return mid
             if target > nums[mid]:
                 l = mid + 1
-            else: #if target < nums[mid]
-                r = mid - 1
-        
+            else: #targer < nums[mid]
+                r = mid -1
         return l
-                
             
                 
                 
