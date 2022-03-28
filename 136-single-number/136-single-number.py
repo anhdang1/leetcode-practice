9445dc4e204240690370c329519d28bda7e054f0
Anh Dang <97132887+anhdang1@users.noreply.github.com>
2022-03-27 23:07:17 -0700

    Time: 150 ms (56.64%), Space: 15.6 MB (80.59%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -4,7 +4,13 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        """
         for i in nums:
             x = nums.count(i)
             if x == 1:
                 return i
+        """
+        res = 0
+        for i in nums:
+            res = i ^ res
+        return res
