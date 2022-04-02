14352b9e042d5f49012ae4bb8a36d8c9ed6fe624
Anh Dang <97132887+anhdang1@users.noreply.github.com>
2022-04-01 23:01:09 -0700

    Time: 1626 ms (16.30%), Space: 15.4 MB (91.90%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -9,8 +9,17 @@ class Solution:
             x = nums.count(i)
             if x == 1:
                 return i
-        """
+        
         res = 0
         for i in nums:
             res = i ^ res
         return res
+        """
+        stack = []
+        for i in nums:
+            if i in stack:
+                stack.remove(i)
+            else:
+                stack.append(i)
+        for a in stack:
+            return a
