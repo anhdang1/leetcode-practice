34fd021a3a49da5dab3d091251fd4ae1a3ea20da
Anh Dang <97132887+anhdang1@users.noreply.github.com>
2022-04-19 23:46:10 -0700

    Time: 1498 ms (22.90%), Space: 0B (100.00%) - LeetHub

---
+++ 607-sales-person/607-sales-person.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+-- names of all sales persons who did not have any orders related to the company named "RED"
+
+-- left join from the salesperson table
+
+SELECT s.name
+FROM salesperson s
+WHERE s.sales_id not in (SELECT o.sales_id
+                        FROM orders o
+                        INNER JOIN company c
+                        ON o.com_id = c.com_id
+                        WHERE c.name = "RED");
